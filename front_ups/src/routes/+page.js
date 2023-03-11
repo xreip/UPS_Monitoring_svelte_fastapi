fcf0967f8ab72d6c7b7b0a624f1e3f72ce77e393
xreip <pierre.mouffe@gmail.com>
2023-03-11 12:24:09 +0100

    lazy and not lazy data fetch + SPA

--- front_ups/src/routes/+page.js
+++ front_ups/src/routes/+page.js
@@ -1,5 +1,14 @@
 export const load = async ({ fetch }) => {
-	const upsRes = await fetch('http://127.0.0.1:8000/ups');
-	const upsData = await upsRes.json();
-	return { upsData };
+	const fetchUps = async () => {
+		const upsRes = await fetch('http://127.0.0.1:8000/ups');
+		const upsData = await upsRes.json();
+		return upsData;
+	};
+
+	// return { upsData };
+	// return { ups: upsData };
+	return {
+		upsnotlazy: fetchUps(),
+		lazy: { ups: fetchUps() }
+	};
 };
--- front_ups/src/routes/+page.svelte
+++ front_ups/src/routes/+page.svelte
@@ -1,15 +1,9 @@
 <script>
-	import ky from 'ky';
 	import UpsComp from './UpsComp.svelte';
 
-	async function getDataKy() {
-		const response = await ky.get('http://127.0.0.1:8000/ups').json();
-		return response;
-	}
-	let myapidata = getDataKy();
-
 	export let data;
-	const { upsData } = data;
+	const { ups } = data.lazy;
+	const { upsnotlazy } = data;
 </script>
 
 <svelte:head>
@@ -19,12 +13,14 @@
 
 <main class="container mx-auto">
 	<section class="grid gap-4 md:grid-cols-2 2xl:grid-cols-4 3xl:grid-flow-col my-20 px-2">
-		{#each upsData as ups}
+		<!-- {#each upsnotlazy as ups}
 			<UpsComp {ups} />
-		{/each}
+		{/each} -->
 
-		{#await myapidata}
-			<p>...waiting</p>
+		{#await ups}
+			<section class="flex items-center justify-center">
+				<p class="text-3xl text-t_gray_med font-bold">Loading data...</p>
+			</section>
 		{:then dataups}
 			{#each dataups as ups}
 				<UpsComp {ups} />
